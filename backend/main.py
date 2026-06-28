d7acdc8323f050c7704329a48b3357fa62941b18
Srutik Nandaniya <srutik5354@gmail.com>
2026-06-28 19:43:25 +0530

    FIX CORS 2

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import smtplib
 import os
@@ -20,10 +18,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://srutikndn1.onrender.com"
-    ],   # Change later for production if needed
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True, 
 )
 
 class Contact(BaseModel):
@@ -32,17 +30,14 @@ class Contact(BaseModel):
     subject: str
     message: str
 
-@app.post("https://srutikndn.onrender.com//contact")
+@app.post("/contact")
 def contact(data: Contact):
     body = f"""
 Name: {data.name}
-
 Email: {data.email}
-
 Subject: {data.subject}
 
 Message:
-
 {data.message}
 """
 
@@ -57,22 +52,3 @@ Message:
         server.send_message(msg)
 
     return {"success": True}
-
-
-# Serve React static files
-app.mount("/assets", StaticFiles(directory="dist/assets"), name="assets")
-
-# Homepage
-@app.get("/", include_in_schema=False)
-async def home():
-    return FileResponse("dist/index.html")
-
-# React Router support
-@app.get("/{full_path:path}", include_in_schema=False)
-async def react_router(full_path: str):
-    file_path = os.path.join("dist", full_path)
-
-    if os.path.isfile(file_path):
-        return FileResponse(file_path)
-
-    return FileResponse("dist/index.html")
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1060,7 +1060,7 @@
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify(form),
+            body: JSON.stringify(FormData),
           }
         );
     
